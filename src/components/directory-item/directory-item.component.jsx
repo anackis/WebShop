9369a813935470eb91ca82ee331023b4237d710d
Alex <rengarsanj@gmail.com>
2022-12-12 13:20:28 +0200

    12.12.2022_morning

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,15 @@
+
+import { useNavigate } from 'react-router-dom';
 import './directory-item.style.scss';
 import React from 'react'
 
 
 const DirectoryItem = ({category}) => {
-  const {imageUrl, title} = category;
+  const {imageUrl, title, route} = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return (
-    <div  className="directory-item-container">
+    <div  className="directory-item-container" onClick={onNavigateHandler}>
       {/* $ in this case allow us to use string inside another string  */}
       <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}}/>
       <div className="directory-body">
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -12,8 +12,8 @@ const ProductCard = ({product}) => {
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`}/>
       <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+        <span className='name'>{name}:</span>
+        <span className='price'>${price}</span>
       </div>
       <Button buttonType='inverted' onClick={addProductToCart} >Add to card </Button>
     </div>
